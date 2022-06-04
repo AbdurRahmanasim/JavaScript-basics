d41dc4eb368576ebe39d3abc5b45f45109112bdc
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-06-04 21:58:04 +0500

    Add files via upload

---
+++ class 01/app.js
@@ -0,0 +1,89 @@
+// what is variable
+
+// Variable Srores values
+
+
+
+var str = "Hello World"
+
+console.log(str)
+
+
+
+// Data Types In javaScript
+
+
+// 1) PREMITIVE DATA TYPE
+// 2) NON PREMITIVE DATA TYPE
+
+
+// ========== PREMITIVE DATA TYPES ================
+// string
+// Number
+// Boolean
+// undefined
+// null
+// Symbol
+// BigInt
+
+
+
+// ========== NON-PREMITIVE DATA TYPES ================
+
+// Array
+// Object
+
+
+
+
+var str =  "ali"
+console.log(str);
+
+
+
+var num = 70 ;
+console.log(num)
+
+var bool1 = true ;
+console.log(bool1)
+
+var bool2 = false ;
+console.log(bool2)
+
+var sym = Symbol("Hello") ;  // It is always unique
+console.log(sym)
+
+var bigIntCheck = 10000000n ;
+console.log(bigIntCheck)
+
+
+
+
+
+// java script is a interoreted language
+
+
+
+//  javascript runs on brower with the help of javascript Engine
+
+
+// every browser has different js engine
+
+
+// chrome has V8 engine for javascript
+
+
+
+
+// use strict is used to strict the code means if you do any mistake which js by default ignores it renders that mistake
+
+
+
+
+
+
+// Home work ============> BigInt 
+
+
+
+
---
+++ class 02/app.js
@@ -0,0 +1,188 @@
+// var  bigIntCheck = 9007199254740991n
+
+
+// var bigIntCheck  = bigIntCheck + 1n ;
+
+
+
+
+// var bigIntCheck = bigIntCheck + 4n ;
+
+// console.log(bigIntCheck);
+
+
+// var a = 9007199254740991n;
+
+// var b = typeof(a) ;
+
+// console.log(b)
+
+
+
+// var a = 5 ; 
+
+// a = 6
+
+
+// console.log(a)
+
+// var num1 = 40 ;
+// var num2 = "40" ;
+
+
+
+// var sym1 = Symbol(40)
+// var sym2 = Symbol(40)
+
+
+// console.log(sym1 == sym2)
+
+// Logical Operators
+
+
+
+
+// var num1 = 10 ;
+// var num2 = 10 ;
+
+// var num3 = 40 ;
+// var num4 = 40 ;
+
+
+
+
+
+// console.log(  num1 === num2  &&  num3 === num4 && false)
+
+
+
+
+
+// console.log(  false   || false    || false   )
+
+
+
+
+
+// var num1 = 50 ;
+
+// var num2 = "50" ;
+
+
+// console.log(num1 !== num2)
+
+
+
+// JavaScript Operations 
+
+
+
+/*
+
+var a = 6 ;
+var b = 5;
+var c = 7 ;
+var d = 8 ;
+
+*/
+
+
+// alert()
+
+// document.write()
+
+
+
+
+// var checkingBodmas = 1 * 2 + 5 / 2 ;
+                     
+
+
+// console.log(checkingBodmas)
+
+
+
+
+
+// INCREMENT AND DECREMENT 
+
+
+
+// PRE INCREMENT 
+
+// var i = 5 ;
+
+// console.log(++i)
+
+
+
+
+// POST INCREMENT 
+
+
+// var j = 10 ;
+
+
+// console.log(j++)
+
+
+// console.log(j)
+
+
+
+// PRE DECREMENT 
+
+// var a =  3 ;
+
+// console.log(--a)
+
+
+// POST DECREMENT 
+
+
+// var b = 4 ;
+
+// console.log(b--)
+
+// console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
