94143593af8cebe81023d3a48713850e6bbd13ba
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-06-12 11:53:24 +0500

    Add files via upload

---
+++ class 03/app.js
@@ -0,0 +1,242 @@
+
+
+// CLASS 3
+
+
+// var num1= "Haniya" ; 
+
+// var num2 = "Fahad" ; 
+
+
+
+// console.log(num1 + num2)
+
+// var str1 =  10 ;
+// var str2 = "20" ;
+
+
+// console.log(str1 + str2)
+
+
+// var str3 =  2 ;
+// var str4 = "2" ;
+
+
+// console.log(str3 * str4)
+
+
+
+
+
+// var str5 =  2 ;
+// var str6 = "2" ;
+
+
+// console.log(str5 / str6)
+
+
+
+
+
+// var str7 =  2 ;
+// var str8 = "2" ;
+
+
+// console.log(str7 - str8)
+
+
+
+
+// var str9 =  2 ;
+// var str10 = "2" ;
+
+
+// console.log(str9 % str10)
+
+
+
+
+
+
+
+// var a = "Ali" ; 
+
+// var b = "Ahmed" ; 
+
+
+
+// // concat function 
+
+
+// var c = a.concat(b) ;
+
+
+// console.log(c)
+
+
+
+
+
+// var name = "Haniya"
+
+// name += "Fahad"
+
+
+
+// console.log(name)
+
+
+
+
+
+// var userName = prompt("Enter Name") ;
+
+
+
+// document.write( typeof(userName) )
+
+
+
+
+
+
+// var userNumber = +prompt("Enter Name") ;
+
+
+
+// document.write(userNumber) 
+
+// console.log(typeof(userNumber))
+
+
+
+
+
+
+
+// var checkNum = NaN ;
+
+// console.log(checkNum) ;
+
+
+
+
+// var checkNum1 = isNaN("hello") ;
+
+
+// console.log(checkNum1)
+
+
+
+// STRING TO NUMBER  
+
+
+// var j = "50" ; 
+
+
+
+// var strToNum = Number(j)
+
+
+
+// console.log(typeof(j))
+
+
+// console.log(typeof(strToNum))
+
+
+
+
+// var k = "90" ; 
+
+
+
+// var strToNum = parseInt(k)
+
+
+
+// console.log(typeof(k))
+
+
+// console.log(typeof(strToNum))
+
+
+
+
+
+// NUMBER TO STRING 
+
+
+
+
+// var num = 10 ;
+
+
+
+// var numToStr = num.toString()
+
+
+// console.log(typeof(num))
+
+
+// console.log(typeof(numToStr))
+
+
+
+
+
+// CASES
+
+
+
+
+
+// var upper = "abdur rahman asim"
+
+// var lower = "HANIYA"
+
+
+// console.log(   upper.toUpperCase()     )
+
+
+// console.log(   lower.toLowerCase()        )
+
+
+
+
+
+
+
+
+
+
+
+
+// var cap = "learning"
+
+
+// var y = cap[0].toUpperCase() + cap.substring( 1,8 )
+
+
+
+// console.log(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
