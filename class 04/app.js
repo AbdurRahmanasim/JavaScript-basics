632319024c820ec7bbbe8d37b5c3df07c9989bef
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-06-14 22:38:24 +0500

    Add files via upload

---
+++ class 04/app.js
@@ -0,0 +1,213 @@
+
+
+// var str1 = "HANIYA"
+
+
+// console.log(  str1.toLowerCase() )
+
+
+
+// var name = "ali"
+
+
+
+
+// index number always start from 0
+
+
+// length always start from 1
+
+
+// console.log(       name[0].toUpperCase() + name.substring( 1  , 3  )           )
+
+
+
+
+
+
+
+// CONDITIONS 
+
+
+// if 
+// else if
+// else
+
+
+
+// if( condition  ){
+
+
+//     block of statement 
+
+
+
+// }
+
+
+
+// var num1 = 10 ;
+
+// var num2 = 10 ; 
+
+
+
+
+// if( num1 === num2 ){
+
+//     console.log("Hello World")
+
+
+// }
+// else{
+
+//     console.log("Hello Haniya")
+
+// }
+
+
+
+// var num = 98
+
+
+
+
+// console.log(a)
+
+var str = "" ;
+
+
+
+// if( str ){
+
+//     console.log("I am True")
+
+
+// }
+// else{
+
+//     console.log("I am False")
+
+// }
+
+
+
+
+
+
+
+// var num =  "10" ;
+
+
+
+// if( num === 10){
+
+// console.log("I am IF")
+
+
+// }
+
+// else if( num === 20 ){
+
+//     console.log("I am ELSE IF")
+
+// }
+
+
+// else{
+
+//     console.log("I am ELSE")
+
+// }
+
+
+
+
+
+
+
+// Calculator
+
+
+
+
+
+
+// var num1 = +prompt("Enter First Number" , "Enter First Number" )
+
+// var opt = prompt("Enter Operator" , "Enter Operator" ) 
+
+// var num2 = +prompt("Enter Second Number" ,  "Enter Second Number")
+
+
+
+
+
+// if( opt === "+" ){
+
+//     document.write(num1 + num2)
+
+
+// }
+
+
+// else if( opt === "-" ){
+
+//     document.write(num1 - num2)
+
+
+// }
+
+// else if( opt === "*" ){
+
+//     document.write(num1 * num2)
+
+
+// }
+
+// else if( opt === "/" ){
+
+//     document.write(num1 / num2)
+
+
+// }
+
+// else if( opt === "%" ){
+
+//     document.write(num1 % num2)
+
+
+// }
+
+
+
+
+// else{
+
+//     document.write(' Syntax Error')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
