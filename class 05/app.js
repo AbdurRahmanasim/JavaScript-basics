4bb328b4efb8166cabd1526c050a973b281971f6
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-07-30 23:42:19 +0500

    Add files via upload

---
+++ class 05/app.js
@@ -0,0 +1,421 @@
+
+
+
+
+// if(condition){
+
+// block of statement
+
+// }
+
+// var str = ""
+
+
+// var num = 7
+
+
+
+// if(num){
+
+// console.log("i am True")
+
+// }
+// else{
+
+
+//     console.log("I am False")
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+// if => conditional statement 
+
+// else if => for multiple conditions
+
+// else ==> if all the conditions are false then else will run 
+
+
+// var key = 96
+
+
+// if( key >= 95 ){
+
+
+//     console.log(" i am Here ")
+
+
+// }
+
+// else if( key > 96 ){
+
+
+// console.log("I am not here")
+
+
+// }
+// else{
+
+//     console.log("Error")
+
+// }
+
+
+
+
+
+// comparision operators :-
+
+// 1) > greater than 
+// 2) < less than
+// 3) >= greate and equals to
+// 4) <= less thana and equals to
+
+
+
+
+// console.log(  59  >=   54 )
+
+// console.log(  50  <=  60  )
+
+
+
+
+
+
+// Nested If
+
+
+
+// if(false){
+
+
+//     if(true){
+
+
+//         console.log("I am Nested If")
+
+
+//     }
+
+
+// }
+
+
+
+
+// var x =  16
+// var y = 98
+// var z =  98
+
+// if(x === 16 ){
+  
+//          console.log("hello haniya")
+
+//     if( y > 0 ){
+
+//           console.log("hello World")
+
+
+//         if(z <= 89){
+
+
+//            console.log("Good Morning")
+
+//             if("s"){
+
+//            console.log("Hii")
+
+//             }
+//     }
+// }
+// }
+
+
+
+
+
+
+// legal and illegal variabel names
+
+
+
+// 1) variable name cant contain any spaces
+// 2) a variabel name can contain only letters , numbers , dollars , underscores , dashes
+// 3) variable cant be of any js keywords
+// 4) capital letters are fine but be carful js is case sensitive
+// 5) please adopt the camel case convention
+
+
+// var NAME = "haNIYA"
+
+// alert(NAME)
+
+
+
+
+// ARRAY
+
+// to store multiple data
+
+// var arr =  [ "Car" , "Bike" , "Home" , "Mobile" ]
+
+
+// , is used to separate the values
+
+
+// console.log(arr)
+
+
+// 
+// index number always starts from 0
+// length always starts from 1
+
+
+// console.log(  arr[3] )
+
+
+// console.log(  arr[4]  )
+
+
+
+// var arr = [];
+
+
+
+// arr[0]  = "Bird"
+
+// arr[1] = "Animal"
+
+// console.log(arr)
+
+
+
+// var birds = [ "piegon" , "crow" , "parrot" , "peacock" , "eagle" , "sparrow" ]
+
+
+// console.log( birds[5] )
+
+
+// console.log( birds[1] )
+
+
+
+
+
+
+
+// one more method is used to create array by using new keyword
+
+
+
+
+// var cars = new Array( "BMW" , "Civic" , "mazda" , "Mercedes" )
+
+
+// console.log(cars)
+
+
+
+
+// console.log(cars[1])
+
+
+
+
+// 6 main methods of Array
+
+
+// 1) Pop
+// 2) Push
+// 3) shift
+// 4) unshift
+// 5) slice
+// 6) splice
+
+
+
+// var arr = [ "haniya" , "Abdur rahman" , "ali"]
+
+
+// var birds = [ "piegon" , "crow" , "parrot" , "peacock" , "eagle" , "sparrow" ]
+
+// birds.pop()
+
+
+// birds.pop()//
+// birds.pop()
+// 
+ 
+
+// console.log(birds)
+
+
+
+
+
+
+
+
+// pop => removes one element from last
+
+
+
+
+
+
+
+//  var birds = [ "piegon" , "crow" , "parrot" , "peacock" , "eagle" , "sparrow" ]
+
+
+
+//  birds.push( "owl" , "woodpecker" )
+
+
+//  console.log(birds)
+
+
+
+
+
+// var key = [ 1,2,3,4,5,6,7 ]
+
+// key.push(8,9,10,11)
+
+
+// console.log(key)
+
+
+// var birds = [ "piegon" , "crow" , "parrot" , "peacock" , "eagle" , "sparrow" ]
+
+
+
+
+
+// birds.shift()
+// birds.shift()
+
+
+// console.log(birds)
+
+
+
+// var birds = [ "piegon" , "crow" , "parrot" , "peacock" , "eagle" , "sparrow" ]
+
+
+// birds.unshift("owl" , "woodpecker" , "seagul" )
+
+
+// console.log(birds)
+
+
+
+
+
+// var birds = [ "piegon" , "crow" , "parrot" , "peacock" , "eagle" , "sparrow" ]
+
+
+
+// birds.splice(2,3)
+
+
+// console.log(birds)
+
+
+// 
+// var num = [ 0,1 ,2,3,4,5,6,7,8,9,10]
+
+// num.splice(4,4)
+
+
+// console.log(num)
+
+
+
+
+
+
+// var num = [ 0,1 ,2,3,4,5,6,7,8,9,10,11,12,12,14,15,16,17]
+
+
+
+// num.splice(11,5)
+
+
+// console.log(num)
+
+
+
+
+
+
+
+// var birds = [ "piegon" , "crow" , "parrot" , "peacock" , "eagle" , "sparrow" ]
+
+
+// birds.splice(2,3,"owl")
+
+
+// console.log(birds)
+
+
+
+// splice(index number , removing length , adding elements)
+
+
+
+
+
+
+// var birds = [ "piegon" , "crow" , "parrot" , "peacock" , "eagle" , "sparrow" ]
+
+
+
+
+// slice is used for making a copy
+
+// var newBirds = birds.slice( 1 , 3 )
+
+// console.log(newBirds)
+
+
+
+
+
+// splice is used to add and delete
+
+
+
+ 
+
+//  var num = [ 0,1 ,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+
+
+
+//  var copy = num.slice(7,15)
+
+//  console.log(copy)
+
+// assignment
+
+
+// 10
+// 11
+// 12
+// 13
+// 14
+
+
+
+
+
+
+
