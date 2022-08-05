7c54d6270dc545386fefde427e65f8a07755e298
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-08-06 02:38:04 +0500

    Add files via upload

---
+++ class 06/app.js
@@ -0,0 +1,423 @@
+
+
+
+// Calcualtor
+
+
+// let num1 = +prompt("Enter First Number")
+// let opt = prompt("Enter Operator")
+// let num2 = +prompt("Enter Second Number")
+
+
+
+
+
+// if(opt === "+"){
+
+
+//     let result = num1 + num2
+
+//     document.write(result)
+// }
+// else if(opt === "-"){
+
+
+//     let result = num1 - num2
+
+//     document.write(result)
+// }
+// else if(opt === "*"){
+
+
+//     let result = num1 * num2
+
+//     document.write(result)
+// }
+// else if(opt === "/"){
+
+
+//     let result = num1 / num2
+
+//     document.write(result)
+// }
+// else if(opt === "%"){
+
+
+//     let result = num1 % num2
+
+//     document.write(result)
+// }
+// else{
+//     alert("Typing Error")
+// }
+
+
+
+
+
+
+
+
+
+// For Loop
+
+
+
+
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+
+
+
+
+// for( initialization ; condition ; increment ){
+
+//     block of statement
+
+// }
+
+
+
+// for( var i = 0  ;  i < 15 ; i++ ){
+
+
+
+//    document.write( "Haniya" , "<br />" )
+
+
+
+// }
+
+
+
+// for(var i = 0 ; i < 21 ; i++){
+
+
+//     document.write("Hello World" + "<br>"  )
+
+
+// }
+
+
+
+
+
+
+// console.log( "Abdur" , "Rahman"  )
+
+
+
+
+
+
+
+
+// let birds = ["piegon" ,"crow" , "parrot" , "eagle" , "peacock" ]
+
+
+// for(var i = 0 ; i < birds.length ; i++ ){
+
+
+//     document.write(birds[i] , "<br>" )
+
+
+// }
+
+
+
+// 2nd Assignment
+
+// let names = ["haniya" , "amil" , "tara" , "isha" , "tamara"]
+
+// let name = prompt("Enter UserName")
+
+// var flag = false
+
+// for(var i = 0 ; i < names.length ; i++){
+
+
+//     if( name === names[i] ){
+
+//         alert("Success")
+
+//         flag = true
+//         break ;
+
+//     }
+  
+
+// }
+
+// if( flag === false  ){
+
+
+//     alert("Incorrect Name")
+
+// }
+
+
+
+
+
+// table creator
+
+
+
+
+
+// let table = prompt("Enter Table Number")
+
+
+
+
+// for(var i = 1 ; i <= 10 ; i++){
+
+
+//     let result = table * i
+
+
+//     document.write( table , " X " , i ,  " = " , result , "<br>"  )
+
+
+// }
+
+
+
+
+
+
+
+// function
+
+
+
+
+// function add(){
+
+
+//     console.log("I am Function")
+
+
+// }
+
+
+
+
+// add()
+
+
+
+// function foo(){
+
+
+
+//     document.write("Haniya")
+
+
+// }
+
+
+// foo()
+
+
+
+
+
+
+
+
+// function add(){
+
+
+
+//     var a = 5 ;
+
+//     var b = 9 ;
+    
+//     var result = a + b ;
+
+//     document.write(result)
+
+
+// }
+
+// add()
+
+
+
+
+
+
+// function has 2 more things 
+// 1) parameter
+// 2) argument
+
+
+
+
+
+// function  key(parameter){
+
+
+   
+
+
+// }
+
+
+// key(argument)
+
+
+
+
+
+// function  key(a){
+
+
+//     console.log(a)
+  
+   
+
+
+// }
+
+
+// key(10)
+
+
+
+// function haniya(str){
+
+
+
+//     console.log(str)
+
+
+
+// }
+
+
+
+
+// haniya("Hello World")
+
+
+
+
+
+// function add( a , b  ){
+
+
+//     console.log(a)
+//     console.log(b)
+
+
+
+// }
+
+// add( 5 )
+
+
+
+
+
+
+// function sub(  a , b , c    ){
+
+
+
+// console.log(  a + b + c   )
+
+
+// }
+
+
+// sub(  8 , 9 , 10 )
+
+
+
+
+// function kuchBhi(  a = 8 , b = 9   ){
+
+
+//     console.log(a)
+//     console.log(b)
+
+
+// }
+
+
+// kuchBhi()
+
+
+
+
+
+
+
+
+// function kuchBhi(  a = 8 , b = 9   ){
+
+
+//     console.log(a)
+//     console.log(b)
+
+
+// }
+
+
+// kuchBhi( 5 , 6  )
+
+
+
+
+
+
+
+
+
+
+// function multiply( a = 2 , b , c = 9 , d = 4 , e , f = 10    ){
+
+
+// console.log( a + b + c + d + e + f )
+
+
+// }
+
+// multiply( 5 , 6, 7, 8 , 8 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
