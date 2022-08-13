cd970e0f50500bbebaa01225cb2c87c7e537488d
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-08-13 23:44:48 +0500

    Add files via upload

---
+++ class 08/app.js
@@ -0,0 +1,288 @@
+
+
+// 17)repeat
+
+
+// var name = "Haniyah"
+
+
+// var check = name.repeat(5)
+
+// document.write(check)
+
+
+// 18) split 
+
+
+// var name = "Haniyah Syeda"
+
+
+// var check = name.split("a")
+
+// console.log(check)
+
+
+
+// var key = "Hello I am here to Study"
+
+
+// var result = key.split(" ")
+
+// console.log(result)
+
+
+
+// 19)slice 
+
+
+// var name = "Haniyah"
+
+
+// var check = name.slice(4,7)
+
+// document.write(check)
+
+
+// 20) toString()
+
+// var num = 50 ;
+
+
+// var str = num.toString() ;
+
+
+// console.log(typeof(num))
+
+// console.log(typeof(str))
+
+
+// 21) substr
+
+
+// var name = "Hello World"
+
+// var check = name.substr(0,5)
+
+// console.log(check)
+
+
+
+// var key = "Youtube is Great"
+
+// var check = key.substr(8,2)
+
+
+// console.log(check)
+
+
+
+// 22) substring
+
+
+
+// var name = "Haniyah"
+
+// var check = name.substring(2,5)
+
+// console.log(check)
+
+
+
+
+// var key = "Good Morning"
+
+
+// var check = key.substring(5,12)
+
+
+// console.log(check)
+
+// 23)valueOf
+
+
+
+// var name = "Haniya"
+
+// var key = name.valueOf()
+
+
+// console.log(key)
+
+
+// var name = "Haniya"
+
+// var check = name.charCodeAt(2)
+
+// console.log(check)
+
+
+
+
+
+
+// Number Methods
+
+
+// 1) Number
+
+
+
+
+// var num = "109" ;
+
+
+// var check = Number(num)
+
+// console.log(typeof(num))
+
+// console.log(typeof(check))
+
+
+// 2)parseInt
+
+
+// var a = "99"
+
+// var b = parseInt(a)
+
+// console.log(typeof(b))
+
+
+// 3)parseFloat()
+
+
+// var num = "55"
+
+// var check = parseFloat(num)
+
+
+// console.log(typeof(check))
+
+
+// 4) isFinite
+
+
+// var num = Infinity
+
+
+// var check = isFinite(num) ;
+
+
+// console.log(check)
+
+
+// 5) isInteger
+
+// var num = 10
+
+
+// var check = Number.isInteger(num)
+
+// console.log(check)
+
+
+
+
+// 6) toFixed
+
+
+// var num = 5.6574393
+
+
+// var check = num.toFixed(2)
+
+
+// console.log(check)
+
+
+
+
+// var key = 1040.5841394781
+
+
+// var check = key.toFixed(9)
+
+// console.log(check)
+
+
+// 7) toPrecision 
+
+
+// var num = 1.34742346
+
+// var check = num.toPrecision(4)
+
+
+// console.log(check)
+
+
+
+// var key = 5047.14234
+
+
+// var check = key.toPrecision(8)
+
+// console.log(check)
+
+
+
+
+
+
+
+// Confirm Dialog Box
+
+// var result = confirm("HEllo World")
+// document.write(result)
+
+
+
+
+
+
+
+// HTML in JavaScript
+
+
+
+// var html = "<ul><li>Element 01</li><li>Element 02</li><li>Element 03</li></ul>" 
+
+
+// document.write(html)
+
+
+
+
+
+// Dates in JavaScript
+
+
+
+// var now = new Date()
+
+
+// console.log(now)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
