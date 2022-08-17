0862cdcbf4da2e59429887c1a654d6655a0aac90
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-08-17 23:40:58 +0500

    Add files via upload

---
+++ class 09/app.js
@@ -0,0 +1,302 @@
+// var now = new Date();
+
+
+// console.log(now)
+
+
+
+
+// Date Methods
+
+
+// 1) toDateString()
+
+
+// var check = now.toDateString()
+
+// document.write(check)
+
+
+// 2) .getDate()
+
+
+// var check = now.getDate();
+
+
+// console.log(check)
+
+
+// 3) .getFullYear()
+
+
+
+// var check = now.getFullYear();
+
+
+// console.log(check)
+
+
+// 4) .getDay()
+
+
+// var check = now.getDay();
+
+
+// console.log(check)
+
+
+// 5)//  var check = now.getMonth();
+
+// console.log(check)
+
+
+// var now = new Date('December 10 2010');
+
+// console.log(now)
+
+
+
+
+
+// var now = new Date();
+
+
+// var check = now.getHours();
+
+
+// console.log(check)
+
+
+
+// var check = now.getMinutes();
+
+// console.log(check)
+
+
+
+
+// var check = now.getSeconds();
+
+// console.log(check)
+
+
+
+
+
+// var check = now.getMilliseconds();
+
+// console.log(check)
+
+
+
+// Now we will use set Methods
+
+
+
+
+
+
+
+// var now = new Date();
+
+
+// now.setFullYear(2025)
+
+// console.log(now)
+
+
+
+// var now = new Date();
+
+
+// now.setHours(15)
+
+// console.log(now)
+
+
+
+// var now = new Date();
+
+
+// now.setMinutes(50)
+
+// console.log(now)
+
+// var now = new Date();
+
+
+// now.setSeconds(50)
+
+// console.log(now)
+
+
+// var now = new Date();
+
+
+// now.setMilliseconds(30)
+
+// console.log(now)
+
+
+
+// var now = new Date();
+
+
+// now.setMonth(2)
+
+// console.log(now)
+
+
+
+
+
+
+// Important getTime Method
+
+
+// return Milliseconds from 1st January 1970
+
+// var now = new Date()
+
+
+// var check = now.getTime()
+
+// console.log(check)
+
+
+
+
+
+
+
+
+// Math Methods
+
+
+// 1) ceil
+
+
+
+// var a = Math.ceil(-5.2);
+
+// ceil increases 
+
+// console.log(a)
+
+
+
+// 2) floor
+
+
+// var a = Math.floor(9.8);
+
+// floor decreases
+
+
+// console.log(a)
+
+
+
+// var b = Math.floor(-18.4)
+
+
+// console.log(b)
+
+// var b = Math.ceil(-18.4)
+
+
+// console.log(b)
+
+
+// 3) round
+
+
+// var a = Math.round(4.67)
+
+// round off
+
+
+// console.log(a)
+
+
+
+// var a = Math.trunc(9.9)
+// removes decimal values
+
+// console.log(a)
+
+
+
+// 4) max
+
+
+
+
+// var check = Math.max(5,10,60,80,100,55)
+
+// console.log(check)
+
+
+
+
+// var check = Math.max(5,10,-60,-80,100,-55)
+
+// console.log(check)
+
+
+// 5)min
+
+
+
+// var check = Math.min(10,11,-11,-10)
+
+// console.log(check)
+
+
+
+
+
+
+// 6) sqrt square root
+
+
+
+// var j = Math.sqrt(4)
+
+
+// console.log(j)
+
+
+
+
+
+
+// 7) random
+
+
+// var check = Math.random();
+
+
+// document.write(check)
+
+
+
+
+// var check = Math.floor(Math.random() * 10) + 1
+
+// document.write(check)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
