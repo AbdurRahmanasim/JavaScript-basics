4e08e690849afbbd1b39f0141af5f77d5533476e
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-08-21 23:41:31 +0500

    Add files via upload

---
+++ class 10/app.js
@@ -0,0 +1,107 @@
+
+
+
+
+
+
+// while loop
+
+
+// do while loop
+
+
+
+
+
+// var i = 0 ;
+
+// while(i < 5){
+
+//     document.write("Haniya" , "<br>")
+
+//     i = i+1
+
+// }
+
+
+
+
+
+
+
+// var i = 0 ;
+
+// do{
+
+//     document.write("Haniya" , "<br>")
+
+//     i = i+1
+
+// }while(i <= 0)
+
+
+
+
+// var num = 0
+
+// if(num  ===  1){
+
+//     document.write("true")
+// }
+// else{
+//     document.write("False")
+// }
+
+
+
+// Switch case
+
+
+// var num = 1 ;
+
+
+// switch(num){
+
+//     case 0 :
+
+//     alert("False")
+
+//     break ;
+
+//     case 1 : 
+
+//     alert("True")
+  
+//     break;
+
+//     default :
+
+//     alert("Error")
+
+// }
+
+
+// var day = 4 
+
+
+// Events
+
+
+
+
+
+
+
+
+
+
+
+
+function show(){
+
+
+    document.write("Hello World")
+
+}
+
+
