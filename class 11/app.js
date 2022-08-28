6d50db470c531a0c10e648f49d113d07366afb39
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-08-28 23:36:43 +0500

    Add files via upload

---
+++ class 11/app.js
@@ -0,0 +1,83 @@
+
+
+
+// function hello(){
+
+
+//     confirm("Do you like our Website")
+
+// }
+
+
+// Reading Field Values
+
+
+// DOM
+
+
+
+// var gettingValue = document.getElementById("heading").innerHTML
+
+// console.log(gettingValue)
+
+
+
+
+
+// function inputvalue(){
+
+//     var inputValue = document.getElementById("typing").value
+
+
+//     console.log(inputValue)
+// }
+
+
+
+// function change(){
+
+
+//     var key = document.getElementById("heading")
+
+//     key.innerHTML = "Hello Haniya"
+
+
+
+// }
+
+
+
+
+
+// function swap(){
+
+//     var key = document.getElementById("picture")
+
+//     key.src = "./pic2.jpg"
+
+
+// }
+
+
+
+
+
+function turnon(){
+
+var key = document.getElementById("image")
+
+key.src = "./bulb1.jpg"
+
+
+}
+
+
+function turnoff(){
+
+    var key = document.getElementById("image")
+    
+    key.src = "./bulb2.jpg"
+    
+    
+    }
+    
