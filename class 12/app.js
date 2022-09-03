36d16acd25671e790244540b4e55fe772cde5454
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-09-03 23:36:11 +0500

    Add files via upload

---
+++ class 12/app.js
@@ -0,0 +1,61 @@
+// var getTag = document.getElementsByTagName("h1") 
+
+// console.log(getTag)
+
+
+// var key = document.getElementsByTagName("p")
+
+// console.log(key)
+
+
+
+// var key = document.getElementsByTagName("li")
+
+// console.log(key[0].innerHTML)
+// console.log(key[2].innerHTML)
+
+
+
+
+
+// var get = document.getElementById("sp").innerHTML
+
+// console.log(get)
+
+
+
+
+// var head = document.getElementById("heading");
+
+// head.className = "head"
+
+
+
+// var sat = document.getElementById("saturday")
+
+// sat.className += " sun"
+
+
+
+// function blue(){
+
+//     var box = document.getElementById("box") ;
+
+//     box.className = "blue"
+
+
+// }
+
+// function green(){
+
+//     var box = document.getElementById("box") ;
+
+//     box.className = "green"
+
+
+// }
+
+
+var head = document.getElementById("heading")
+
+head.style.backgroundColor = "red"
