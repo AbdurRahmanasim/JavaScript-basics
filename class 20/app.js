f47e335a21a258bd18f279e8b6e3a64bf9814b55
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-10-16 22:35:38 +0500

    Add files via upload

---
+++ class 20/app.js
@@ -0,0 +1,161 @@
+
+
+// var str = "Ali" ;
+
+// str = "Haniya" ; 
+
+// console.log( str )
+
+
+// var num = 10 ;
+
+// var num = 20 ;
+
+// console.log( num )
+
+
+// let name = "Haniya" ;
+
+// name = "abdur rahman" ;
+// console.log(name)
+
+
+// let str = "abdur rahman" ;
+
+// let str = "haniya" ;
+
+// console.log(str)
+
+
+// const x = 10 ; 
+
+// x = 20 ; 
+
+// console.log(x)
+
+
+// const a = 10 ;
+// const a = 20 ;
+
+// console.log(a) ;
+
+
+// if(true){
+
+// let str = "ali" ;
+
+// }
+
+// console.log(str)
+
+
+// Template String
+
+// var str = "Haniya" ;
+
+// document.write(`My name is ${str}`)
+
+// Ternary Operator
+
+
+// Condition ? console.log("True") : console.log("False") ; 
+
+// var num1 = 10 ;
+// var num2 = 20 ;
+// var num3 = 10 ;
+// var num4 = 20 ;
+
+
+// num1 == num3 && num2 == num4 ? console.log("Hello World") : console.log("JavaScript") ; 
+
+
+// let random = Math.floor(Math.random() * 2) + 1 ;
+// console.log(random);
+
+// console.log(str)
+
+// var str = "Abdur Rahman" ;
+
+
+// Lexical Scoping
+
+// parent canot access chils dar ;
+// child can access parent 
+
+
+// lexical scoping
+
+function call(){
+
+    // var a = "Hello" ;
+    // console.log(a)
+    console.log(h)
+    nested()
+    function nested(){
+        var h = "Hii"
+    }
+}
+
+call()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
