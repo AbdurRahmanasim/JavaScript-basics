cc486d878d989a6482e50d18af218254a6441ccf
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-10-16 22:35:57 +0500

    Add files via upload

---
+++ class 21/app.js
@@ -0,0 +1,141 @@
+// var str;
+
+// Variable Hoisting
+
+
+// console.log(str)
+
+
+// var str = "Ahmed" ;
+
+
+// Lexical Scoping
+
+
+// if(true){
+
+//     var name = "ali" ;
+    // console.log(name)
+    // console.log(str)
+
+    // if(true){
+
+        // var str = "Haniya";
+        // console.log(str)
+//         console.log(name)
+
+//     }
+
+
+// }
+
+
+// Normal Function ES5 function
+
+// function foo(){
+
+//     console.log("Hello World")
+
+// }
+
+// foo()
+
+
+// Fat Arrow Function
+
+
+// const add = (a) =>{ 
+//     console.log(a)
+
+// } ;
+
+// add(1890)
+
+// Anonymous Function
+
+
+// var func = function(){
+//     console.log("Hello World")
+// }
+// func()
+
+
+// Callback Functions
+
+
+// var hello = ()=>{console.log("Dinner")} ;
+
+// hello()
+
+
+// Spread Operator
+
+
+// var arr = [ "crow" , "eagle" , "piegon" , "sparrow" ] ;
+
+// console.log( ...arr )
+
+
+// Destructing of object
+
+// var obj = {
+
+//     name : "Ali" ,
+//     age : 20 
+
+// }
+
+
+
+// let { name , age } = obj ;
+
+
+// console.log(age)
+
+
+
+// Destructuring or Array
+
+
+// let arr =  [ "crow" , "eagle" , "piegon" , "sparrow" ] ;
+
+
+// console.log(arr[3])
+
+
+// let [ a , b , c , d ] = arr;
+
+
+// console.log(c)
+  
+// Swapping Variable
+
+// let x = "Hello" ; 
+// let y = 55 ; 
+
+// [x,y] = [y,x] ;
+
+// console.log( y )
+// console.log( x )
+
+// Assignment
+// 01) For in loop
+// 02) for of loop
+// 03) for each loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
