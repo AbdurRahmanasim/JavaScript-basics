1c6b0fe018a3456debb9230c1bfc33ebe2b5f5e8
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-11-26 00:30:36 +0500

    Add files via upload

---
+++ class-29/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+  <>
+   <h1 className="heading">Hello World</h1>
+
+   <h1>Hello Class</h1>
+
+   <h1>Good Morning</h1>
+
+   <br />
+   <input type="text" />
+
+   <h1>Hello World</h1>
+  </>
+  );
+}
+
+export default App;
